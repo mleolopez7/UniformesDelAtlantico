e2da5bee3172567f2db53922c0314db9b8df58c1
Leonardo Lopez <mleolopez7@gmail.com>
2024-02-12 22:38:40 -0600

    Update index.php
    regreso

--- Views/index.php
+++ Views/index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Iniciar Sesiónnn</title>
+        <title>Iniciar Sesión</title>
         <link href="<?php echo base_url; ?>Assets/css/styles.css" rel="stylesheet" />
         <script src="<?php echo base_url; ?>Assets/js/all.min.js" crossorigin="anonymous"></script>
     </head>
